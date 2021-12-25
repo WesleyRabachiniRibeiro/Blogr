535f66f21dde5f1c2d872c3793ba75b3f825f9b2
WesleyRabachiniRibeiro <wesleyrr2015@gmail.com>
2021-12-25 15:13:38 -0300

    created the script of Dropdown Menu

--- index.js
+++ index.js
@@ -23,3 +23,66 @@ function addImgs(element) {
 
 addImgs(editorMobile);
 addImgs(laptopMobile);
+
+// Dropdown Menu
+document.querySelectorAll(".menu").forEach(menu => {menu.addEventListener('click', (event) =>{
+    const drop1 = document.querySelector('#drop1')
+    const drop2 = document.querySelector('#drop2')
+    const drop3 = document.querySelector('#drop3')
+    const products = document.querySelector('#product')
+    const company = document.querySelector('#company')
+    const connect = document.querySelector('#connect')
+    switch(event.target.id){
+        case "product":
+            if(drop1.classList.toggle('d-none')){
+                drop1.classList.add('d-none')
+                products.classList.remove('up-arrow')
+                products.classList.add('down-arrow')
+            }
+            else{
+                drop1.classList.remove('d-none')
+                products.classList.add('up-arrow')
+                products.classList.remove('down-arrow')
+                drop2.classList.add('d-none')
+                company.classList.remove('up-arrow')
+                company.classList.add('down-arrow')
+                drop3.classList.add('d-none')
+            }
+            break
+        case "company":
+            if(drop2.classList.toggle('d-none')){
+                drop2.classList.add('d-none')
+                company.classList.remove('up-arrow')
+                company.classList.add('down-arrow')
+            }
+            else{
+                drop2.classList.remove('d-none')
+                company.classList.add('up-arrow')
+                company.classList.remove('down-arrow')
+                drop1.classList.add('d-none')
+                products.classList.remove('up-arrow')
+                products.classList.add('down-arrow')
+                drop3.classList.add('d-none')
+            }
+            break
+        case "connect":
+            if(drop3.classList.toggle('d-none')){
+                drop3.classList.add('d-none')
+                connect.classList.remove('up-arrow')
+                connect.classList.add('down-arrow')
+            }
+            else{
+                drop3.classList.remove('d-none')
+                connect.classList.add('up-arrow')
+                connect.classList.remove('down-arrow')
+                drop1.classList.add('d-none')
+                products.classList.remove('up-arrow')
+                products.classList.add('down-arrow')
+                drop2.classList.add('d-none')
+                company.classList.remove('up-arrow')
+                company.classList.add('down-arrow')
+            }
+            break
+    }
+})
+});
