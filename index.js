f689cf02a3bc86234c399bd01285cfd14e49dac8
WesleyRabachiniRibeiro <wesleyrr2015@gmail.com>
2021-12-27 13:10:51 -0300

    test

--- index.js
+++ index.js
@@ -1,114 +1,114 @@
-// Creating of the Imgs in Mobile of dinamic form
-const editorMobile = {
-    'id' : 'designed',
-    'img' :  './images/illustration-editor-mobile.svg',
-    'position' : 1
-}
+// // Creating of the Imgs in Mobile of dinamic form
+// const editorMobile = {
+//     'id' : 'designed',
+//     'img' :  './images/illustration-editor-mobile.svg',
+//     'position' : 1
+// }
 
-const laptopMobile = {
-    'id' : 'explanation',
-    'img' :  './images/illustration-laptop-mobile.svg',
-    'position' : 0
-}
+// const laptopMobile = {
+//     'id' : 'explanation',
+//     'img' :  './images/illustration-laptop-mobile.svg',
+//     'position' : 0
+// }
 
-function addImgs(element) {
-    const selectedElement = document.getElementById(`${element.id}`);
-    const newPicture = document.createElement("picture");
-    const newImg = document.createElement("img");
-    newPicture.setAttribute('class','mobile');
-    newImg.src = element.img;
-    newPicture.appendChild(newImg);
-    selectedElement.insertBefore(newPicture, selectedElement.children[element.position]);
-}
+// function addImgs(element) {
+//     const selectedElement = document.getElementById(`${element.id}`);
+//     const newPicture = document.createElement("picture");
+//     const newImg = document.createElement("img");
+//     newPicture.setAttribute('class','mobile');
+//     newImg.src = element.img;
+//     newPicture.appendChild(newImg);
+//     selectedElement.insertBefore(newPicture, selectedElement.children[element.position]);
+// }
 
-addImgs(editorMobile);
-addImgs(laptopMobile);
+// addImgs(editorMobile);
+// addImgs(laptopMobile);
 
-// Dropdown Menu
-document.querySelectorAll(".menu").forEach(menu => {menu.addEventListener('click', (event) =>{
-    const drop1 = document.querySelector('#drop1')
-    const drop2 = document.querySelector('#drop2')
-    const drop3 = document.querySelector('#drop3')
-    const products = document.querySelector('#product')
-    const company = document.querySelector('#company')
-    const connect = document.querySelector('#connect')
-    switch(event.target.id){
-        case "product":
-            if(drop1.classList.toggle('d-none')){
-                drop1.classList.add('d-none')
-                products.classList.remove('up-arrow')
-                products.classList.add('down-arrow')
-            }
-            else{
-                drop1.classList.remove('d-none')
-                products.classList.add('up-arrow')
-                products.classList.remove('down-arrow')
-                drop2.classList.add('d-none')
-                company.classList.remove('up-arrow')
-                company.classList.add('down-arrow')
-                drop3.classList.add('d-none')
-            }
-            break
-        case "company":
-            if(drop2.classList.toggle('d-none')){
-                drop2.classList.add('d-none')
-                company.classList.remove('up-arrow')
-                company.classList.add('down-arrow')
-            }
-            else{
-                drop2.classList.remove('d-none')
-                company.classList.add('up-arrow')
-                company.classList.remove('down-arrow')
-                drop1.classList.add('d-none')
-                products.classList.remove('up-arrow')
-                products.classList.add('down-arrow')
-                drop3.classList.add('d-none')
-            }
-            break
-        case "connect":
-            if(drop3.classList.toggle('d-none')){
-                drop3.classList.add('d-none')
-                connect.classList.remove('up-arrow')
-                connect.classList.add('down-arrow')
-            }
-            else{
-                drop3.classList.remove('d-none')
-                connect.classList.add('up-arrow')
-                connect.classList.remove('down-arrow')
-                drop1.classList.add('d-none')
-                products.classList.remove('up-arrow')
-                products.classList.add('down-arrow')
-                drop2.classList.add('d-none')
-                company.classList.remove('up-arrow')
-                company.classList.add('down-arrow')
-            }
-            break
-        }
-    })
-});
+// // Dropdown Menu
+// document.querySelectorAll(".menu").forEach(menu => {menu.addEventListener('click', (event) =>{
+//     const drop1 = document.querySelector('#drop1')
+//     const drop2 = document.querySelector('#drop2')
+//     const drop3 = document.querySelector('#drop3')
+//     const products = document.querySelector('#product')
+//     const company = document.querySelector('#company')
+//     const connect = document.querySelector('#connect')
+//     switch(event.target.id){
+//         case "product":
+//             if(drop1.classList.toggle('d-none')){
+//                 drop1.classList.add('d-none')
+//                 products.classList.remove('up-arrow')
+//                 products.classList.add('down-arrow')
+//             }
+//             else{
+//                 drop1.classList.remove('d-none')
+//                 products.classList.add('up-arrow')
+//                 products.classList.remove('down-arrow')
+//                 drop2.classList.add('d-none')
+//                 company.classList.remove('up-arrow')
+//                 company.classList.add('down-arrow')
+//                 drop3.classList.add('d-none')
+//             }
+//             break
+//         case "company":
+//             if(drop2.classList.toggle('d-none')){
+//                 drop2.classList.add('d-none')
+//                 company.classList.remove('up-arrow')
+//                 company.classList.add('down-arrow')
+//             }
+//             else{
+//                 drop2.classList.remove('d-none')
+//                 company.classList.add('up-arrow')
+//                 company.classList.remove('down-arrow')
+//                 drop1.classList.add('d-none')
+//                 products.classList.remove('up-arrow')
+//                 products.classList.add('down-arrow')
+//                 drop3.classList.add('d-none')
+//             }
+//             break
+//         case "connect":
+//             if(drop3.classList.toggle('d-none')){
+//                 drop3.classList.add('d-none')
+//                 connect.classList.remove('up-arrow')
+//                 connect.classList.add('down-arrow')
+//             }
+//             else{
+//                 drop3.classList.remove('d-none')
+//                 connect.classList.add('up-arrow')
+//                 connect.classList.remove('down-arrow')
+//                 drop1.classList.add('d-none')
+//                 products.classList.remove('up-arrow')
+//                 products.classList.add('down-arrow')
+//                 drop2.classList.add('d-none')
+//                 company.classList.remove('up-arrow')
+//                 company.classList.add('down-arrow')
+//             }
+//             break
+//         }
+//     })
+// });
 
 
-// Hamburger Menu
-const hamburgerMenu = document.querySelector('#hamburger')
-const menu = document.querySelector('#initial-menu')
-hamburgerMenu.addEventListener('click', () => {
-    if(hamburgerMenu.src.indexOf('/images/icon-hamburger.svg') != -1){
-        hamburgerMenu.src = './images/icon-close.svg'
-        menu.classList.remove('d-none')
-    }else{
-        hamburgerMenu.src = './images/icon-hamburger.svg'
-        menu.classList.add('d-none')
-    }
-})
+// // Hamburger Menu
+// const hamburgerMenu = document.querySelector('#hamburger')
+// const menu = document.querySelector('#initial-menu')
+// hamburgerMenu.addEventListener('click', () => {
+//     if(hamburgerMenu.src.indexOf('/images/icon-hamburger.svg') != -1){
+//         hamburgerMenu.src = './images/icon-close.svg'
+//         menu.classList.remove('d-none')
+//     }else{
+//         hamburgerMenu.src = './images/icon-hamburger.svg'
+//         menu.classList.add('d-none')
+//     }
+// })
 
-setInterval( () =>{
-    if(window.innerWidth <= 1200 && hamburgerMenu.src.indexOf('/images/icon-close.svg') == -1){
-        menu.classList.add('d-none')
-    }else if(window.innerWidth > 1200){
-        hamburgerMenu.src = './images/icon-hamburger.svg'
-        menu.classList.remove('d-none')
-    }
-    else{
-        menu.classList.remove('d-none')
-    }
-}, 0)
+// setInterval( () =>{
+//     if(window.innerWidth <= 1200 && hamburgerMenu.src.indexOf('/images/icon-close.svg') == -1){
+//         menu.classList.add('d-none')
+//     }else if(window.innerWidth > 1200){
+//         hamburgerMenu.src = './images/icon-hamburger.svg'
+//         menu.classList.remove('d-none')
+//     }
+//     else{
+//         menu.classList.remove('d-none')
+//     }
+// }, 0)
